16f7753e28809d9132a0b00f8a89d3626e3f5c0a
franco <fluffins007@outlook.com>
2026-05-22 09:52:14 -0400

    Added kcopy krename and kexists. Fixed logic involving permissions. | 1.0.1

--- kernel.py
+++ kernel.py
@@ -2,18 +2,14 @@ import file
 
 UID = 1
 STDERR = 0
-VERSION = "1.0.0"
+VERSION = "1.0.1"
+NAME = "PyX Kernel"
 TRUSTED = []
 
 def kerror(error_message,error_code=1):
     global STDERR
     print(f"Kernel: Error, {error_message}")
     STDERR = error_code
-
-def kinfo():
-    print("Python kernel")
-    print(f"Version {VERSION}")
-    print("Created and maintained by SrFluff")
 
 def kwrite(file_name, file_content):
     if UID == 0 or UID in TRUSTED:
@@ -26,11 +22,38 @@ def kread(file_name):
     if file_name not in data:
         kerror("No such file.")
     else:
-        print(data[file_name])
+        return data[file_name]
+
+def kexists(file_name):
+    return file_name in file.rraw()
 
 def kremove(file_name):
     if UID == 0 or UID in TRUSTED:
-        if file.remove(file_name) == 1:
-            kerror("No such file.")
+        if not kexists(file_name):
+            kerror(f"'{file_name}' no such file.")
+        else:
+            file.remove(file_name)
+    else:
+        kerror("UID is not allowed to write.")
+
+def krename(original_name,new_name):
+    if UID == 0 or UID in TRUSTED:
+        if not kexists(original_name):
+            kerror(f"'{original_name}' no such file.")
+        elif kexists(new_name):
+            kerror(f"'{new_name}' exists.")
+        else:
+            file.rename(original_name,new_name)
+    else:
+        kerror("UID is not allowed to write.")
+
+def kcopy(original_name, new_name):
+    if UID == 0 or UID in TRUSTED:
+        if not kexists(original_name):
+            kerror(f"'{original_name}' no such file.")
+        elif kexists(new_name):
+            kerror(f"'{new_name}' exists.")
+        else:
+            file.copy(original_name,new_name)
     else:
         kerror("UID is not allowed to write.")
--- shell.py
+++ shell.py
@@ -1,12 +1,29 @@
+import json
+from shlex import split
+
 import kernel
 import std
 
-from shlex import split
-
 kernel.UID = 1
+VERSION = "1.0.0"
+print(f"PSH v{VERSION}")
+print(f"Kernel: {kernel.NAME} v{kernel.VERSION}")
+
+config = {
+        "name":"user",
+        "host":"psh"
+        }
+
+if kernel.kexists("shell.conf"):
+    data = kernel.kread("shell.conf")
+    try:
+        config = json.loads(data)
+    except Exception:
+        std.error("Shell","Parsing shell.conf failed.")
+
 while True:
     if kernel.STDERR == 0:
-        a = input("> ")
+        a = input(f"{config['name']}@{config['host']} # ")
     else:
         a = input(f"[{kernel.STDERR}]> ")
     kernel.STDERR = 0
@@ -27,20 +44,34 @@ while True:
             kernel.kwrite(file_name,"")
         elif com == "cat":
             file_name = split(a)[1]
-            kernel.kread(file_name)
+            if kernel.kread(file_name) != None:
+                print(kernel.kread(file_name))
         elif com == "chid":
             if split(a)[1].isdigit():
                 kernel.UID = int(split(a)[1])
             else:
                 std.error("Shell","invalid character for UID.")
+        elif com == "rm":
+            file_name = split(a)[1]
+            kernel.kremove(file_name)
     elif len(split(a)) == 3:
         com = split(a)[0]
         if com == "write":
             file_name = split(a)[1]
-            kernel.kwrite(file_name,split(a)[2])
+            kernel.kwrite(file_name,split(a)[2].replace("\\n","\n"))
+        elif com == "mv":
+            original_name = split(a)[1]
+            new_name = split(a)[2]
+            kernel.krename(original_name,new_name)
+        elif com == "cp":
+            original_name = split(a)[1]
+            new_name = split(a)[2]
+            kernel.kcopy(original_name,new_name)
     elif len(split(a)) >= 2:
         com = split(a)[0]
         if com == "rm":
+            print("AAA")
             for i in split(a)[1:]:
+                print(i)
                 file_name = i
                 kernel.kremove(file_name)
